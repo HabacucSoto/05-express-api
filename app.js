f6132b2ad6373e655dcec3cb444133a8771a7627
habacuc <josehabacuc182020@gmail.com>
2022-04-29 09:13:56 -0500

    modified app.js

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require( 'express' )
+const app = express()
+const port = 3000
+
+// ! Indicando que se usara JSON
+app.use( express.json() )  
+
+// ! HTTP methods
+app.get( '/v1/explorers', ( req, res ) => {
+
+    console.log( `API explorers (get all requests) ${ new Date() }`)
+
+    const explorer1 = { id: 1, name: 'Habacuc1' }
+    const explorer2 = { id: 2, name: 'Habacuc2' }
+    const explorer3 = { id: 3, name: 'Habacuc3' }
+    const explorer4 = { id: 4, name: 'Habacuc4' }
+
+    const explorers = [ explorer1, explorer2, explorer3, explorer4 ]
+
+    res.status( 200 ).json( explorers )
+
+} )
+
+app.get( '/v1/explorers/:id', ( req, res ) => {
+
+    console.log( `API explorers GET request ${ new Date() }` )
+    console.log( `Getting explorer with id: ${ req.params.id }` )
+
+    const explorer = { id: 1, name: 'Habacuc' }
+    res.status( 200 ).json( explorer )
+
+} )
+
+app.post( '/v1/explorers', ( req, res ) => {
+
+    console.log( `API explorers POST request ${ new Date() }` )
+
+    const requestBody = req.body // ! Parametros de un cliente
+    res.status( 201 ).json( { message: 'created' } )
+
+} )
+
+app.put( '/v1/explorers/:id', ( req, res ) => {
+
+    console.log( `API explorers PUT request ${ new Date() }` )
+    console.log( `Update explorer with id: ${ req.params.id }` )
+
+    const requestBody = req.body // ! Parametros de un cliente
+    res.status( 200 ).json( { message: 'updated' } )
+
+} )
+
+app.delete( '/v1/explorers/:id', ( req, res ) => {
+
+    console.log( `API explorers DELETE request ${ new Date() }` )
+    console.log( `Delete explorer with id: ${ req.params.id }` )
+
+    const requestBody = req.body // ! Parametros de un cliente
+    res.status( 200 ).json( { message: 'deleted' } )
+
+} )
+
+app.listen( port, () => console.log( `server: ${ port }` ) )
